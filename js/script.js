0c102328de0e2ed317218bddbffb2e01eb70951c
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2017-12-13 17:59:39 -0200

    Adição de novos arquivos e commit teste

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	$("#btn_open_menu_lateral").click(function(){
-		$("#menu_esquerda").animate({width:'toggle'},200);
+		$("#container_menu_esquerda").animate({width:'toggle'},200);
 	});
 
-	$("#menu_esquerda").mouseleave(function(){
-		$("#menu_esquerda").animate({width:'toggle'},200);
+	$("#container_menu_esquerda").mouseleave(function(){
+		$("#container_menu_esquerda").animate({width:'toggle'},200);
 	});	
 });
